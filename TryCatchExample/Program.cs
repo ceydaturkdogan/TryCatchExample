a1aba643e416784279c7aaef6a8fd5c3ca9ee507
Ceyda Turkdogan <ceyda.turkdogan@gmail.com>
2024-08-21 17:40:06 +0300

    Add project files.

---
+++ TryCatchExample/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+try
+{
+    Console.WriteLine("Lütfen karesini almak istediğiniz sayıyı giriniz");
+
+    int number = Convert.ToInt32(Console.ReadLine());
+
+    double square=Math.Pow(2, number);
+
+    Console.WriteLine($"Sonuç: {square}");
+}
+
+catch
+{
+    throw new Exception("Geçersiz giriş! Lütfen bir sayı giriniz.");
+}
